e5495dc38ac079296d8dddfbfb66562d3006cc04
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-20 11:24:12 +0530

    working with list comprehension

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ number=[1,2,3]
 # new_list=new_item for item in list
 new_list=[n+1 for n in number]
 print(new_list)
+
+name= "Sharath"
+
+name_list= [val for val in name]
+print(name_list)
+
+number_list=[n*2 for n in range(1,5)]
+print(number_list)
+# new_list=new_item for item in list if test
+names=["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+
+short_name=[val for val in names if len(val)<5]
+print(short_name)
+
+uppercase_name=[val.upper() for val in names if len(val)>5 ]
+print(uppercase_name)
